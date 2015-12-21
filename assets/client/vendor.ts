dd70a2128be00374cc9998a8a65a45cf16ba357a
bob <bob@vynca.org>
2015-12-21 14:47:42 +0800

    modify: add bootstrap.js into vendor

--- assets/client/vendor.ts
+++ assets/client/vendor.ts
@@ -9,6 +9,7 @@ import 'angular2/core';
 import 'angular2/router';
 import 'angular2/http';
 import 'rxjs';
+import 'bootstrap-sass/assets/javascripts/bootstrap.js';
 
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import 'font-awesome/scss/font-awesome.scss';
